fd43fa0128d048fc545eff86ee5779fe69b8518b
FranciscaDuarte <a2019126141@isec.pt>
2023-12-07 19:04:33 +0000

    Create client FIFO

--- src/jogoUI.c
+++ src/jogoUI.c
@@ -1,6 +1,7 @@
 #include "jogoUI.h"
 
 #pragma region Funções auxiliares
+
 void handler_saSIGWINCH(int sigNum, siginfo_t *info, void *old) {
   int newHeight, newWidth;
   getmaxyx(stdscr, newHeight, newWidth);
@@ -8,13 +9,19 @@ void handler_saSIGWINCH(int sigNum, siginfo_t *info, void *old) {
   refresh();
   clear();
 
-  mvprintw(1, 10, "____________________________________________________"); // mensagem fora da janela, na linha 1, coluna 10 do ecrã
-  mvprintw(2, 10, "[                     Bem-vindo                    ]"); // mensagem fora da janela, na linha 2, coluna 10 do ecrã
-  mvprintw(3, 10, "[                   Testa as setas!                ]"); // mensagem fora da janela, na linha 3, coluna 10 do ecrã
-  mvprintw(4, 10, "[ space - muda para o teste de comandos / q - sair ]"); // mensagem fora da janela, na linha 4, coluna 10 do ecrã
-  mvprintw(5, 10, "____________________________________________________"); // mensagem fora da janela, na linha 5, coluna 10 do ecrã
+  mvprintw(1, 10,
+           "____________________________________________________"); // mensagem fora da janela, na linha 1, coluna 10 do ecrã
+  mvprintw(2, 10,
+           "[                     Bem-vindo                    ]"); // mensagem fora da janela, na linha 2, coluna 10 do ecrã
+  mvprintw(3, 10,
+           "[                   Testa as setas!                ]"); // mensagem fora da janela, na linha 3, coluna 10 do ecrã
+  mvprintw(4, 10,
+           "[ space - muda para o teste de comandos / q - sair ]"); // mensagem fora da janela, na linha 4, coluna 10 do ecrã
+  mvprintw(5, 10,
+           "____________________________________________________"); // mensagem fora da janela, na linha 5, coluna 10 do ecrã
 
-  janelaMapa = newwin(17, 40, 6, 16);  // Criar janela para a matriz de jogo, tendo os parametro numero de linhas,numero de colunas, posição onde começa a janela  e posição onde termina
+  janelaMapa = newwin(17, 40, 6,
+                      16);  // Criar janela para a matriz de jogo, tendo os parametro numero de linhas,numero de colunas, posição onde começa a janela  e posição onde termina
   janelaComandos = newwin(5, 40, 25, 1);
 
   desenhaJanela(janelaMapa, 2);  // função exemplo que desenha o janela no ecrã
@@ -25,64 +32,55 @@ void handler_saSIGWINCH(int sigNum, siginfo_t *info, void *old) {
   refresh();
 }
 
-void desenhaJanela(WINDOW *janela, int tipo) // Função que desenha o mapa
-{
-  if (tipo == 1)
-  {
+// Função que desenha o mapa
+void desenhaJanela(WINDOW *janela, int tipo) {
+  if (tipo == 1) {
     wprintw(janela, "Bem-vindo %s!", playerName); // escreve a palavra UP na janelaTopo
     scrollok(janela, TRUE); // liga o scroll na "janela".
-  }
-  else
-  {
+  } else {
     keypad(janela, TRUE); // para ligar as teclas de direção (aplicar à janela)
     wclear(janela);// limpa a janela
-    wborder(janela, '|', '|', '-', '-', '+', '+', '+', '+'); // Desenha uma borda. Nota importante: tudo o que escreverem, devem ter em conta a posição da borda
+    wborder(janela, '|', '|', '-', '-', '+', '+', '+',
+            '+'); // Desenha uma borda. Nota importante: tudo o que escreverem, devem ter em conta a posição da borda
   }
   refresh(); // necessário para atualizar a janela
   wrefresh(janela); // necessário para atualizar a janela
 }
 
-void trataTeclado()
-{
+void trataTeclado() {
   keypad(janelaMapa, TRUE);       // para ligar as teclas de direção (aplicar à janelaTopo)
   wmove(janelaMapa, 1, 1);        // posiciona o cursor,visualmente, na posicao 1,1 da janelaTopo
-  int tecla = wgetch(janelaMapa); // MUITO importante: o input é feito sobre a janela em questão, neste caso na janelaTopo
-  char* command = (char *) malloc(sizeof(char) * MAXLEN);
+  int tecla = wgetch(
+    janelaMapa); // MUITO importante: o input é feito sobre a janela em questão, neste caso na janelaTopo
+  char *command = (char *) malloc(sizeof(char) * MAXLEN);
 
   while (tecla != 113) // trata as tecla até introduzirem a letra q. O código asci de q é 113
   {
-
     if (tecla == KEY_UP) // quando o utilizador introduz a seta para cima
     {
       desenhaJanela(janelaMapa, 2); // atualiza toda a janela
       mvwprintw(janelaMapa, 1, 1, "Estou a carregar na tecla UP"); // escreve a palavra UP na janelaTopo
-      wrefresh(janelaMapa);// // necessário para atualizar a janelaTopo. Nota: é apenas esta janela que pretendemos atualizar
-    }
-    else if (tecla == KEY_RIGHT)
-    {
+      wrefresh(
+        janelaMapa);// // necessário para atualizar a janelaTopo. Nota: é apenas esta janela que pretendemos atualizar
+    } else if (tecla == KEY_RIGHT) {
       desenhaJanela(janelaMapa, 2);
       mvwprintw(janelaMapa, 1, 1, "Estou a carregar na tecla RIGHT");
       wrefresh(janelaMapa);
-    }
-    else if (tecla == KEY_LEFT)
-    {
+    } else if (tecla == KEY_LEFT) {
       desenhaJanela(janelaMapa, 2);
       mvwprintw(janelaMapa, 1, 1, "Estou a carregar na tecla LEFT");
       wrefresh(janelaMapa);
-    }
-    else if (tecla == KEY_DOWN)
-    {
+    } else if (tecla == KEY_DOWN) {
       desenhaJanela(janelaMapa, 2);
       mvwprintw(janelaMapa, 1, 1, "Estou a carregar na tecla DOWN");
       wrefresh(janelaMapa);
-    }
-    else if (tecla == ' ') // trata a tecla espaço
+    } else if (tecla == ' ') // trata a tecla espaço
     {
       curs_set(1); // ativa o cursor
       echo();                        // habilita o echo, para que o que o utilizador escreve apareça no ecrã
       wprintw(janelaComandos, "\n #> "); // utilizada para imprimir.
       wgetstr(janelaComandos, command);  // para receber do teclado uma string na "janelaBaixo" para a variavel comando
-      if(commands(command, tecla) == 1) return;
+      if (commands(command, tecla) == 1) return;
 
       noecho(); //voltar a desabilitar o que o utilizador escreve
       wrefresh(janelaComandos); //sempre que se escreve numa janela, tem de se fazer refresh
@@ -90,15 +88,18 @@ void trataTeclado()
     wclear(janelaComandos); // função que limpa o ecrã
     curs_set(0); // faz com que o cursor não seja visivel
     wmove(janelaMapa, 1, 1); // posiciona o cursor (visualmente) na posicao 1,1 da janelaTopo
-    tecla = wgetch(janelaMapa); //espera que o utilizador introduza um inteiro. Importante e como já referido anteriormente introduzir a janela onde queremos receber o input
+    tecla = wgetch(
+      janelaMapa); //espera que o utilizador introduza um inteiro. Importante e como já referido anteriormente introduzir a janela onde queremos receber o input
   }
+  exitCommand();
 }
+
 # pragma endregion
 
 #pragma region Comandos
-int commands(char* command, int tecla)
-{
-  char* commandAux = strtok(command, " \n");
+
+int commands(char *command, int tecla) {
+  char *commandAux = strtok(command, " \n");
   char *username, *msg;
 
   if (commandAux == NULL) // Verificação da correta utilização do programa.
@@ -106,57 +107,65 @@ int commands(char* command, int tecla)
     return 0;
   }
 
-  if(!strcmp(commandAux, "players"))  // Comando players
+  if (!strcmp(commandAux, "players"))  // Comando players
   {
     playersCommand();
-  }
-  else if(!strcmp(commandAux, "msg")) // Comando msg
+  } else if (!strcmp(commandAux, "msg")) // Comando msg
   {
     username = strtok(NULL, " ");
     msg = strtok(NULL, " ");
-    if(username == NULL || msg == NULL) // Verificação da correta utilização do comando.
-      wprintw(janelaComandos,"[ERRO] Syntax: msg <username> <message>\n");
-    else
-    {
-      wprintw(janelaComandos,"username: %s\n", username);
-      wprintw(janelaComandos,"msg: %s\n", msg);
+    if (username == NULL || msg == NULL) // Verificação da correta utilização do comando.
+      wprintw(janelaComandos, "[ERRO] Syntax: msg <username> <message>\n");
+    else {
+      wprintw(janelaComandos, "username: %s\n", username);
+      wprintw(janelaComandos, "msg: %s\n", msg);
       msgCommand(username, msg);
     }
-  }
-  else if(!strcmp(commandAux, "exit"))  // Comando exit
+  } else if (!strcmp(commandAux, "exit"))  // Comando exit
   {
     exitCommand();
     strcpy(command, "exit");
     return 1;
-  }
-  else wprintw(janelaComandos,("[ERRO]: Comando invalido.\n"));
+  } else wprintw(janelaComandos, ("[ERRO]: Comando invalido.\n"));
 }
 
-void playersCommand() // Comando players
-{
-  wprintw(janelaComandos,"\nComando [players] nao implementado.\n");
+// Comando players
+void playersCommand() {
+  wprintw(janelaComandos, "\nComando [players] nao implementado.\n");
 }
 
-void msgCommand(char *username, char* msg) // Comando msg
-{
-  wprintw(janelaComandos,"\nComando [msg] nao implementado.\n");
+// Comando msg
+void msgCommand(char *username, char *msg) {
+  wprintw(janelaComandos, "\nComando [msg] nao implementado.\n");
 }
 
-void exitCommand() // Comando exit
-{
-  wprintw(janelaComandos,"\nComando [exit] nao implementado.\n");
+// Comando exit
+void exitCommand() {
+  // TODO Change this
+  char clientFIFO_Name[22];
+  sprintf(clientFIFO_Name, CLIENT_FIFO, getpid());
+  unlink(clientFIFO_Name);
+  wprintw(janelaComandos, "\nComando [exit] nao implementado.\n");
 }
+
 #pragma endregion
 
-int main(int argc, char* argv[], char* envp[])
-{
-  if(argc != 2)
-  {
+int main(int argc, char *argv[], char *envp[]) {
+  if (argc != 2) {
     printf("[ERRO]: Introduza o nome do jogador como argumento.\n");
     exit(1);
   }
 
   playerName = argv[1];
+
+  // Criação do FIFO do Cliente
+  char clientFIFO_Name[22];
+  sprintf(clientFIFO_Name, CLIENT_FIFO, getpid());
+  int result = mkfifo(clientFIFO_Name, 0777);
+  if (result == -1) {
+    printf("[ERRO]: Erro ao criar FIFO do Cliente %d", getpid());
+    exit(-1);
+  }
 
   struct sigaction saSIGWINCH;
   saSIGWINCH.sa_sigaction = handler_saSIGWINCH;
@@ -168,13 +177,19 @@ int main(int argc, char* argv[], char* envp[])
   noecho();  // desliga o echo no ecrã, para voltar ativar devem utilizar a função echo();
   keypad(stdscr, TRUE);  // habilita as teclas  especiais, exemplo -> <-
 
-  mvprintw(1, 10, "____________________________________________________"); // mensagem fora da janela, na linha 1, coluna 10 do ecrã
-  mvprintw(2, 10, "[                      Bem-vindo                   ]"); // mensagem fora da janela, na linha 2, coluna 10 do ecrã
-  mvprintw(3, 10, "[                   testa as setas!                ]"); // mensagem fora da janela, na linha 3, coluna 10 do ecrã
-  mvprintw(4, 10, "[ space - muda para o teste de comandos / q - sair ]"); // mensagem fora da janela, na linha 4, coluna 10 do ecrã
-  mvprintw(5, 10, "____________________________________________________"); // mensagem fora da janela, na linha 5, coluna 10 do ecrã
+  mvprintw(1, 10,
+           "____________________________________________________"); // mensagem fora da janela, na linha 1, coluna 10 do ecrã
+  mvprintw(2, 10,
+           "[                      Bem-vindo                   ]"); // mensagem fora da janela, na linha 2, coluna 10 do ecrã
+  mvprintw(3, 10,
+           "[                   testa as setas!                ]"); // mensagem fora da janela, na linha 3, coluna 10 do ecrã
+  mvprintw(4, 10,
+           "[ space - muda para o teste de comandos / q - sair ]"); // mensagem fora da janela, na linha 4, coluna 10 do ecrã
+  mvprintw(5, 10,
+           "____________________________________________________"); // mensagem fora da janela, na linha 5, coluna 10 do ecrã
 
-  janelaMapa = newwin(17, 40, 6, 16);  // Criar janela para a matriz de jogo, tendo os parametro numero de linhas,numero de colunas, posição onde começa a janela  e posição onde termina
+  janelaMapa = newwin(17, 40, 6,
+                      16);  // Criar janela para a matriz de jogo, tendo os parametro numero de linhas,numero de colunas, posição onde começa a janela  e posição onde termina
   janelaComandos = newwin(5, 40, 25, 1);
 
   desenhaJanela(janelaMapa, 2);  // função exemplo que desenha o janela no ecrã
--- src/motor.c
+++ src/motor.c
@@ -2,90 +2,77 @@
 
 #pragma region Curses Commands
 
-int commandsCurses(char* command, int tecla){
+int commandsCurses(char *command, int tecla) {
 
-  char* commandAux = strtok(command, " \n");
-  char* arg;
+  char *commandAux = strtok(command, " \n");
+  char *arg;
 
   if (commandAux == NULL) // Verificação da correta utilização do programa.
   {
     return 0;
   }
 
-  if(!strcmp(commandAux, "users")) // Comando users.
+  if (!strcmp(commandAux, "users")) // Comando users.
   {
     usersCommandCurses();
-  }
-  else if(!strcmp(commandAux, "kick")) // Comando kick.
+  } else if (!strcmp(commandAux, "kick")) // Comando kick.
   {
     arg = strtok(NULL, " ");
-    if(arg == NULL) // Verificação da correta utilização do comando kick.
+    if (arg == NULL) // Verificação da correta utilização do comando kick.
       wprintw(janelaComandos, "[ERRO] Syntax: kick <username>\n");
-    else
-    {
-      wprintw(janelaComandos,"username: %s\n", arg);
+    else {
+      wprintw(janelaComandos, "username: %s\n", arg);
       kickCommandCurses(arg);
     }
-  }
-  else if(!strcmp(commandAux, "bots")) // Comando bots.
+  } else if (!strcmp(commandAux, "bots")) // Comando bots.
   {
     botsCommandCurses();
-  }
-  else if(!strcmp(commandAux, "bmov")) // Comando bmov.
+  } else if (!strcmp(commandAux, "bmov")) // Comando bmov.
   {
     bmovCommandCurses();
-  }
-  else if(!strcmp(commandAux, "rbm")) // Comando rbm.
+  } else if (!strcmp(commandAux, "rbm")) // Comando rbm.
   {
     rbmCommandCurses();
-  }
-  else if(!strcmp(commandAux, "begin")) // Comando begin.
+  } else if (!strcmp(commandAux, "begin")) // Comando begin.
   {
     beginCommandCurses();
-  }
-  else if(!strcmp(commandAux, "end")) // Comando end.
+  } else if (!strcmp(commandAux, "end")) // Comando end.
   {
     endCommandCurses();
-  }
-  else if(!strcmp(commandAux, "test_bot")) // Comando test_bot.
+  } else if (!strcmp(commandAux, "test_bot")) // Comando test_bot.
   {
-    char* interval  = strtok(NULL, " ");
-    char* duration = strtok(NULL, " ");
-    if(interval == NULL || duration == NULL)
+    char *interval = strtok(NULL, " ");
+    char *duration = strtok(NULL, " ");
+    if (interval == NULL || duration == NULL)
       wprintw(janelaComandos, "[ERRO] Syntax: test_bot <interval> <duration>\n");
-    else
-    {
-      wprintw(janelaComandos,"interval: %s\n", interval);
-      wprintw(janelaComandos,"duration: %s\n", duration);
+    else {
+      wprintw(janelaComandos, "interval: %s\n", interval);
+      wprintw(janelaComandos, "duration: %s\n", duration);
 
       testBotCommandCurses(interval, duration, tecla);
     }
-  }
-  else wprintw(janelaComandos,("[ERRO]: Comando invalido.\n")); // Comando inválido.
+  } else wprintw(janelaComandos, ("[ERRO]: Comando invalido.\n")); // Comando inválido.
 }
 
 // Função que trata do comando test_bot.
-void testBotCommandCurses(char* interval, char* duration, int tecla) {
+void testBotCommandCurses(char *interval, char *duration, int tecla) {
   int nBytes, state; // nBytes: número de bytes recebidos do bot
   int pipeBotMotor[2]; // pipeBotMotor: pipe para comunicação entre o bot e o motor
 
   // Verificação da correta criação do pipeBotMotor.
-  if (pipe(pipeBotMotor) == -1)
-  {
-    wprintw(janelaComandos,"[ERRO]: Nao foi possível criar o pipe para comunicacao com o Bot.\n");
+  if (pipe(pipeBotMotor) == -1) {
+    wprintw(janelaComandos, "[ERRO]: Nao foi possível criar o pipe para comunicacao com o Bot.\n");
     exit(1);
   }
 
   // Verificação da correta execução do fork.
   botPID[0] = fork();
-  if (botPID[0] == -1)
-  {
-    wprintw(janelaComandos,"[ERRO]: O fork falhou.\n");
+  if (botPID[0] == -1) {
+    wprintw(janelaComandos, "[ERRO]: O fork falhou.\n");
     exit(2);
   }
 
-  if (botPID[0] == 0)
-  {
+  if (botPID[0] == 0) {
     // Bot (Child Process)
 
     // Fecho dos terminais dos pipes que não serão utilizados.
@@ -100,36 +87,34 @@ void testBotCommandCurses(char* interval, char* duration, int tecla) {
     execl("bot", "bot", interval, duration, NULL);
 
     // Verificação da correta execução do execl. Se for bem-sucedido, este código será substituído.
-    wprintw(janelaComandos,"[ERRO]: bot nao encontrado.\n");
+    wprintw(janelaComandos, "[ERRO]: bot nao encontrado.\n");
     exit(3);
-  }
-  else
-  {
+  } else {
     // Motor (Parent process)
-    char* botInfo = (char *) malloc(MAXLEN);
+    char *botInfo = (char *) malloc(MAXLEN);
     union sigval result;
 
-    wprintw(janelaComandos,"Bot PID: %d\n\n", botPID[0]);
+    wprintw(janelaComandos, "Bot PID: %d\n\n", botPID[0]);
 
     // Fecho do terminal do pipe que não será utilizado.
     close(pipeBotMotor[1]);
-    for(int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
       // Recebe a informação do bot.
       nBytes = read(pipeBotMotor[0], botInfo, MAXLEN);
 
-      if(nBytes == 0) {
+      if (nBytes == 0) {
         botPID[0] = -1;
         break;
       }
 
       botInfo[nBytes + 1] = '\0';
 
-      char* xChar = strtok(botInfo, " ");
-      char* yChar  = strtok(NULL, " ");
-      char* dChar = strtok(NULL, "\n");
+      char *xChar = strtok(botInfo, " ");
+      char *yChar = strtok(NULL, " ");
+      char *dChar = strtok(NULL, "\n");
 
       int x = atoi(xChar), y = atoi(yChar), d = atoi(dChar);
-      wprintw(janelaBot,"Recebi: %d %d %d\n\n", x, y, d);
+      wprintw(janelaBot, "Recebi: %d %d %d\n\n", x, y, d);
       wrefresh(janelaComandos);
       wrefresh(janelaBot);
     }
@@ -145,46 +130,48 @@ void testBotCommandCurses(char* interval, char* duration, int tecla) {
   }
 }
 
-void usersCommandCurses() // Função que trata do comando users.
-{
-  wprintw(janelaComandos,"\nComando [users] nao implementado.\n");
+// Função que trata do comando users.
+void usersCommandCurses() {
+  wprintw(janelaComandos, "\nComando [users] nao implementado.\n");
 }
 
-void kickCommandCurses(char *username) // Função que trata do comando kick.
-{
-  wprintw(janelaComandos,"\nComando [kick] nao implementado.\n");
+// Função que trata do comando kick.
+void kickCommandCurses(char *username) {
+  wprintw(janelaComandos, "\nComando [kick] nao implementado.\n");
 }
 
-void botsCommandCurses() // Função que trata do comando bots.
-{
-  wprintw(janelaComandos,"\nComando [bots] nao implementado.\n");
+// Função que trata do comando bots.
+void botsCommandCurses() {
+  wprintw(janelaComandos, "\nComando [bots] nao implementado.\n");
 }
 
-void bmovCommandCurses() // Função que trata do comando bmov.
-{
-  wprintw(janelaComandos,"\nComando [bmov] nao implementado.\n");
+// Função que trata do comando bmov.
+void bmovCommandCurses() {
+  wprintw(janelaComandos, "\nComando [bmov] nao implementado.\n");
 }
 
-void rbmCommandCurses() // Função que trata do comando rbm.
-{
-  wprintw(janelaComandos,"\nComando [rbm] nao implementado.\n");
+// Função que trata do comando rbm.
+
+void rbmCommandCurses() {
+  wprintw(janelaComandos, "\nComando [rbm] nao implementado.\n");
 }
 
-void beginCommandCurses() // Função que trata do comando begin.
-{
-  wprintw(janelaComandos,"\nComando [begin] nao implementado.\n");
+// Função que trata do comando begin.
+void beginCommandCurses() {
+  wprintw(janelaComandos, "\nComando [begin] nao implementado.\n");
 }
 
-void endCommandCurses() // Função que trata do comando end.
-{
+// Função que trata do comando end.
+void endCommandCurses() {
   unlink(SERVER_FIFO);
-  wprintw(janelaComandos,"\nComando [end] nao implementado.\n");
+  wprintw(janelaComandos, "\nComando [end] nao implementado.\n");
 }
 
 #pragma endregion
 
 #pragma region Auxiliar Functions
 
+// Função que trata o sinal de resize do terminal
 void handler_saSIGWINCH(int sigNum, siginfo_t *info, void *old) {
   int newHeight, newWidth;
   getmaxyx(stdscr, newHeight, newWidth);
@@ -192,11 +179,15 @@ void handler_saSIGWINCH(int sigNum, siginfo_t *info, void *old) {
   refresh();
   clear();
 
-  mvprintw(1, 10, "____________________________________________________");  // mensagem fora da janela, na linha 1, coluna 10 do ecrã
-  mvprintw(2, 10, "[ space - muda para o teste de comandos / q - sair ]"); // mensagem fora da janela, na linha 2, coluna 10 do ecrã
-  mvprintw(3, 10, "____________________________________________________"); // mensagem fora da janela, na linha 3, coluna 10 do ecrã
+  mvprintw(1, 10,
+           "____________________________________________________");  // mensagem fora da janela, na linha 1, coluna 10 do ecrã
+  mvprintw(2, 10,
+           "[ space - muda para o teste de comandos / q - sair ]"); // mensagem fora da janela, na linha 2, coluna 10 do ecrã
+  mvprintw(3, 10,
+           "____________________________________________________"); // mensagem fora da janela, na linha 3, coluna 10 do ecrã
 
-  janelaMapa = newwin(17, 40, 4, 16);  // Criar janela para a matriz de jogo, tendo os parametro numero de linhas,numero de colunas, posição onde começa a janela  e posição onde termina
+  janelaMapa = newwin(17, 40, 4,
+                      16);  // Criar janela para a matriz de jogo, tendo os parametro numero de linhas,numero de colunas, posição onde começa a janela  e posição onde termina
   janelaComandos = newwin(5, 40, 21, 1);
 
   desenhaJanela(janelaMapa, 2);  // função exemplo que desenha o janela no ecrã
@@ -209,56 +200,54 @@ void handler_saSIGWINCH(int sigNum, siginfo_t *info, void *old) {
   refresh();
 }
 
-void readMap(int level) { // Função que lê o mapa do ficheiro.
+// Função que lê o mapa do ficheiro.
+void readMap(int level) {
   char filename[20];
   sprintf(filename, "%s%d%s", "../levels/map", level, ".txt");
 
   int fileD = open(filename, O_RDONLY); // Abrir o ficheiro para leitura.
-  if(fileD == -1) { // Verificação da correta abertura do ficheiro.
+  if (fileD == -1) { // Verificação da correta abertura do ficheiro.
     printf("[ERRO]: Erro ao abrir o ficheiro %s", filename);
     perror("");
     exit(1);
   }
   char c;
-  for(int row = 0; row < ROWS; row++) { // Ler o mapa do ficheiro.
-    for(int col = 0; col < COLS; col ++) {
+  for (int row = 0; row < ROWS; row++) { // Ler o mapa do ficheiro.
+    for (int col = 0; col < COLS; col++) {
       read(fileD, &map[row][col], 1);
     }
     read(fileD, &c, 1);
   }
 }
 
-void desenhaJanela(WINDOW *janela, int tipo) // Função que desenha o mapa.
-{
-  if (tipo == 1)
-  {
+// Função que desenha o mapa.
+void desenhaJanela(WINDOW *janela, int tipo) {
+  if (tipo == 1) {
     scrollok(janela, TRUE); // liga o scroll na "janela".
-  }
-  else if (tipo == 2)
-  {
+  } else if (tipo == 2) {
     keypad(janela, TRUE); // para ligar as teclas de direção (aplicar à janela)
     wclear(janela);// limpa a janela
 
-    for(int row = 0; row < ROWS; row++) { // Imprime o mapa na janela.
-      for(int col = 0; col < COLS; col ++) {
-        wprintw(janela,"%c", map[row][col]);
+    for (int row = 0; row < ROWS; row++) { // Imprime o mapa na janela.
+      for (int col = 0; col < COLS; col++) {
+        wprintw(janela, "%c", map[row][col]);
       }
     }
-  }
-  else if(tipo == 3) {
+  } else if (tipo == 3) {
     scrollok(janela, TRUE); // liga o scroll na "janela".
-    if(botPID[0] != -1)
-      wprintw(janelaComandos,"Bot PID: %d\n\n", botPID[0]);
+    if (botPID[0] != -1)
+      wprintw(janelaComandos, "Bot PID: %d\n\n", botPID[0]);
   }
   refresh(); // necessário para atualizar a janela
   wrefresh(janela); // necessário para atualizar a janela
 }
 
-void trataTeclado() // Função que trata do teclado.
-{
+// Função que trata do teclado.
+void trataTeclado() {
   curs_set(0); // faz com que o cursor não seja visivel
-  int tecla = wgetch(janelaMapa); // MUITO importante: o input é feito sobre a janela em questão, neste caso na janelaMapa
-  char* command = (char *) malloc(sizeof(char) * MAXLEN);
+  int tecla = wgetch(
+    janelaMapa); // MUITO importante: o input é feito sobre a janela em questão, neste caso na janelaMapa
+  char *command = (char *) malloc(sizeof(char) * MAXLEN);
 
   while (tecla != 113) // trata as tecla até introduzirem a letra q. O código asci de q é 113
   {
@@ -268,9 +257,10 @@ void trataTeclado() // Função que trata do teclado.
       curs_set(1); // faz com que o cursor seja visivel
       echo();                         // habilita o echo no ecrã, para voltar desativar devem utilizar a função noecho();
       wprintw(janelaComandos, "\n #> "); // utilizada para imprimir.
-      wgetstr(janelaComandos, command);  // para receber do teclado uma string na "janelaComandos" para a variavel comando
+      wgetstr(janelaComandos,
+              command);  // para receber do teclado uma string na "janelaComandos" para a variavel comando
 
-      if(commandsCurses(command, tecla) == 1) return;
+      if (commandsCurses(command, tecla) == 1) return;
 
       noecho(); //voltar a desabilitar o que o utilizador escreve
       wrefresh(janelaComandos); //sempre que se escreve numa janela, tem de se fazer refresh
@@ -279,16 +269,16 @@ void trataTeclado() // Função que trata do teclado.
     wclear(janelaComandos); // função que limpa o ecrã
     curs_set(0); // faz com que o cursor não seja visivel
     wmove(janelaMapa, 1, 1); // posiciona o cursor (visualmente) na posicao 1,1 da janelaMapa
-    tecla = wgetch(janelaMapa); //espera que o utilizador introduza um inteiro. Importante e como já referido anteriormente introduzir a janela onde queremos receber o input
+    tecla = wgetch(
+      janelaMapa); //espera que o utilizador introduza um inteiro. Importante e como já referido anteriormente introduzir a janela onde queremos receber o input
   }
   endCommandCurses();
 }
 
 #pragma endregion
 
-int main(int argc, char* argv[], char* envp[])
-{
-  if(argc != 1) // Verificação da correta utilização do programa.
+int main(int argc, char *argv[], char *envp[]) {
+  if (argc != 1) // Verificação da correta utilização do programa.
   {
     printf("[ERRO]: Argumento invalido.\n");
     exit(1);
@@ -305,7 +295,7 @@ int main(int argc, char* argv[], char* envp[])
 
   // Criação do FIFO do Servidor
   int result = mkfifo(SERVER_FIFO, 0777);
-  if(result == -1) {
+  if (result == -1) {
     printf("[ERRO]: Erro ao criar FIFO do Servidor");
     exit(-1);
   }
@@ -315,11 +305,15 @@ int main(int argc, char* argv[], char* envp[])
   noecho();  // desliga o echo no ecrã, para voltar ativar devem utilizar a função echo();
   keypad(stdscr, TRUE);  // habilita as teclas  especiais, exemplo -> <-
 
-  mvprintw(1, 10, "____________________________________________________");  // mensagem fora da janela, na linha 1, coluna 10 do ecrã
-  mvprintw(2, 10, "[ space - muda para o teste de comandos / q - sair ]"); // mensagem fora da janela, na linha 2, coluna 10 do ecrã
-  mvprintw(3, 10, "____________________________________________________"); // mensagem fora da janela, na linha 3, coluna 10 do ecrã
+  mvprintw(1, 10,
+           "____________________________________________________");  // mensagem fora da janela, na linha 1, coluna 10 do ecrã
+  mvprintw(2, 10,
+           "[ space - muda para o teste de comandos / q - sair ]"); // mensagem fora da janela, na linha 2, coluna 10 do ecrã
+  mvprintw(3, 10,
+           "____________________________________________________"); // mensagem fora da janela, na linha 3, coluna 10 do ecrã
 
-  janelaMapa = newwin(17, 40, 4, 16);  // Criar janela para a matriz de jogo, tendo os parametro numero de linhas,numero de colunas, posição onde começa a janela  e posição onde termina
+  janelaMapa = newwin(17, 40, 4,
+                      16);  // Criar janela para a matriz de jogo, tendo os parametro numero de linhas,numero de colunas, posição onde começa a janela  e posição onde termina
   janelaComandos = newwin(5, 40, 21, 1);
   janelaBot = newwin(3, 40, 26, 1);
   desenhaJanela(janelaMapa, 2);  // função exemplo que desenha o janela no ecrã
